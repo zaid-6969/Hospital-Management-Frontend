2f6035624912d9dc7b8a1d691798b79ee5028925
zaid-6969 <bawasamohammedzaid@gmail.com>
2026-04-13 15:23:17 +0530

    fix app initialization order + cors

--- src/pages/UserPage/components/DoctorCard.jsx
+++ src/pages/UserPage/components/DoctorCard.jsx
@@ -1,12 +1,13 @@
 import { useNavigate } from "react-router-dom";
 
-
 const DoctorCard = ({ doctor, onBook }) => {
-
-   const navigate = useNavigate();
+  const navigate = useNavigate();
 
   return (
-    <div onClick={()=>navigate(`/user/doctor/${doctor._id}`)} className="bg-card rounded-2xl p-5 flex gap-4 shadow hover:-translate-y-1 transition">
+    <div
+      onClick={() => navigate(`/user/doctor/${doctor._id}`)}
+      className="bg-card rounded-2xl p-5 flex gap-4 shadow hover:-translate-y-1 transition"
+    >
       <img
         src={doctor.image?.url}
         alt=""
@@ -16,21 +17,24 @@ const DoctorCard = ({ doctor, onBook }) => {
       <div className="flex flex-col justify-between flex-1">
         <div>
           <h2 className="font-bold text-lg">{doctor.name}</h2>
-          <p className="text-indigo-600 text-sm">
-            {doctor.specialization}
-          </p>
+          <p className="text-indigo-600 text-sm">{doctor.specialization}</p>
 
-          <div className="text-xs mt-2 text-text/60">
-           <p>Experience</p> {doctor.experience}
+          <div className="mt-3 bg-white border border-gray-200 rounded-xl px-4 py-3 shadow-sm flex items-center justify-between">
+            <div>
+              <p className="text-xs text-gray-400 uppercase tracking-wide">
+                Experience
+              </p>
+              <p className="text-sm font-semibold text-gray-800">
+                {doctor.experience} Years Experience
+              </p>
+            </div>
           </div>
         </div>
 
         <div className="flex justify-between items-center mt-4">
           <div>
             <p className="text-xs text-gray-400">Next Available</p>
-            <p className="text-sm font-semibold">
-              {doctor.next}
-            </p>
+            <p className="text-sm font-semibold">{doctor.next}</p>
           </div>
 
           <button
